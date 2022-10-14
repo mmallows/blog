cc719263fa831a09636b59d14c50da6a357282b9
Marcus Mallows <marcusmallows@aol.com>
2022-10-14 18:38:29 -0400

    user login & signin

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BlogListView, BlogDetailView
 
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
 ]
--- django_project/urls.py
+++ django_project/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrig.auth.urls")),
+    path("accounts/", include("accounts.urls")),
     path("", include("blog.urls")),
 ]
